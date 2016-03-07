ea9ad46e8833a73f15f6c388940fcdc522227acb
panicbit <panicbit.dev@gmail.com>
2016-03-07 17:36:54 +0100

    Refactor memory reads

--- src/memory.rs
+++ src/memory.rs
@@ -18,43 +18,21 @@ impl Memory {
     }
 
     pub fn read_u8(&mut self, addr: Addr) -> u8 {
-        // 128 internal RAM (stack)
-        if *addr >= 0xFF80 {
-            self.stack[(*addr - 0xFF80) as usize]
-        } else
-        // Empty
-        if addr.in_range(0xFF4C, 0xFF80) {
-            0
-        } else
-        // I/O ports
-        if addr.in_range(0xFF00, 0xFF4C) {
-            println!("STUB: I/O port access: 0x{:02X}", *addr);
-            0
-        } else
-        // OAM
-        if addr.in_range(0xFE00, 0xFF00) {
-            println!("STUB: OAM access: 0x{:02X}", *addr);
-            0
-        } else
-        // Echo of 8K internal RAM
-        if addr.in_range(0xE000, 0xFE00) {
-            self.ram[(*addr - 0xE000) as usize]
-        } else
-        // 8K internal RAM
-        if addr.in_range(0xC000, 0xE000) {
-            self.ram[(*addr - 0xC000) as usize]
-        } else
-        // Video RAM
-        if addr.in_range(0x8000, 0xA000) {
-            println!("STUB: Video RAM read: 0x{:02X}", *addr);
-            0
-        } else
-        // ROM bank #0
-        if addr.in_range(0x0000, 0x4000) {
-            debug_assert!(self.rom.data.len() >= 0x4000);
-            self.rom.data[addr.0 as usize]
-        } else {
-            panic!("read addr stub: 0x{:02X}", *addr);
+        use self::Location::*;
+        match Location::from_addr(*addr) {
+            InterruptEnable => { println!("READ_STUB: IE register"); 0 }
+            InternalRam128(offset) => self.stack[offset as usize],
+            Empty => 0,
+            SerialPort => { println!("STUB: Serial port stub: 0x{:02X}", *addr); 0 }
+            IOStub => { println!("STUB: I/O port access: 0x{:02X}", *addr); 0 }
+            OAM(_offset) => { println!("STUB: OAM access: 0x{:02X}", *addr); 0 }
+            InternalRam8k(offset) => self.ram[offset as usize],
+            VRAM(_offset) => { println!("STUB: Video RAM read: 0x{:02X}", *addr); 0 }
+            ROM0(offset) => {
+                debug_assert!(self.rom.data.len() >= 0x4000);
+                self.rom.data[offset as usize]
+            }
+            Stub => panic!("read addr stub: 0x{:02X}", *addr)
         }
     }
 
@@ -114,6 +92,39 @@ impl Memory {
 
     pub fn size(&self) -> u16 {
         0xFFFF
+    }
+}
+
+pub enum Location {
+    InterruptEnable,
+    InternalRam128(u16),
+    SerialPort,
+    Empty,
+    IOStub,
+    OAM(u16),
+    InternalRam8k(u16),
+    VRAM(u16),
+    ROM0(u16),
+    Stub,
+}
+
+impl Location {
+    fn from_addr(addr: u16) -> Location {
+        use self::Location::*;
+        match addr {
+            0xFFFF            => InterruptEnable,
+            0xFF80 ... 0xFFFE => InternalRam128(addr - 0xFF80),
+            0xFF4C ... 0xFF7F => Empty,
+            0xFF01            => SerialPort,
+            0xFF00 ... 0xFF4B => IOStub,
+            0xFEA0 ... 0xFEFF => Empty,
+            0xFE00 ... 0xFE9F => OAM(addr - 0xFE00),
+            0xE000 ... 0xFDFF => InternalRam8k(addr - 0xE000),
+            0xC000 ... 0xDFFF => InternalRam8k(addr - 0xC000),
+            0x8000 ... 0x9FFF => VRAM(addr - 0x8000),
+            0x0000 ... 0x3FFF => ROM0(addr),
+            _                 => Stub
+        }
     }
 }
 
