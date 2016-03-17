47590b89ef1a4dcdacf88d031c7e739f25a80235
panicbit <panicbit.dev@gmail.com>
2016-03-17 18:46:55 +0100

    Impl a few LD instructions

--- src/instructions.rs
+++ src/instructions.rs
@@ -140,6 +140,8 @@ instructions! {
     0x5F, 1,  4, LD_E_A => unborrow!(cpu.set_e(cpu.a()));
     0x5A, 1,  4, LD_E_D => unborrow!(cpu.set_e(cpu.d()));
     0x26, 2,  8, LD_H_n(value: u8) => cpu.set_h(value);
+    0x6F, 1,  4, LD_L_A => unborrow!(cpu.set_l(cpu.a()));
+    0x6E, 1,  8, LD_L_MHL => unborrow!(cpu.set_l(mem.read_u8(Addr(cpu.hl()))));
     0x01, 3, 12, LD_BC_nn(value: u16) => cpu.set_bc(value);
     0x11, 3, 12, LD_DE_nn(value: u16) => cpu.set_de(value);
     0x21, 3, 12, LD_HL_nn(value: u16) => cpu.set_hl(value);
