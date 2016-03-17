d29bb815ef02ab191a1e0c416633615fcbb65084
panicbit <panicbit.dev@gmail.com>
2016-03-17 06:57:36 +0100

    Impl OR_MHL

--- src/instructions.rs
+++ src/instructions.rs
@@ -209,6 +209,7 @@ instructions! {
     0xB7, 1,  4, OR_A => unborrow!(cpu.or(cpu.a()));
     0xB0, 1,  4, OR_B => unborrow!(cpu.or(cpu.b()));
     0xB1, 1,  4, OR_C => unborrow!(cpu.or(cpu.c()));
+    0xB6, 1,  8, OR_MHL => unborrow!(cpu.or(mem.read_u8(Addr(cpu.hl()))));
     0xE6, 2,  8, AND_n(value: u8) => cpu.and(value);
     0xAE, 1,  8, XOR_MHL => unborrow!(cpu.xor(mem.read_u8(Addr(cpu.hl()))));
     0xA9, 1,  4, XOR_C => unborrow!(cpu.xor(cpu.c()));
